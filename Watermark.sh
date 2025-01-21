4751f1b97810ef32535e4726afe27b8f7dbfa7b0
Yzz404 <samsung10destop@gmail.com>
2025-01-21 19:08:53 +0700

    Update Watermark.sh

--- Watermark.sh
+++ Watermark.sh
@@ -1,5 +1,5 @@
 # =============================== #
-#              BY Thonxyzz404
+#      BY Thonxyzz404
 # =============================== #
 NFS=/data/data/com.termux/files/usr/tmp/tmp.6mak9eJLKZ
 base32 -d >${NFS}<<THONXYZZ404EOFYZXYRWQQPYXZA
